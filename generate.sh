ee6a53a17b387aac56c65d7b4e325029b35c71eb
Andrew Milkowski <andrewmilkowski@gmail.com>
2010-09-18 16:11:14 -0400

    first commit

--- generate.sh
+++ generate.sh
@@ -1,9 +1,10 @@
 # http://www.symfony-project.org/getting-started/1_4/en/
 
+SYMFONY_HOME=lib/vendor/symfony/branches/1.4
+
 #! --- one time only --
-svn checkout http://svn.symfony-project.com/branches/1.4/ lib/vendor/symfony
+#svn checkout http://svn.symfony-project.com/branches/1.4 ${SYMFONY_HOME}
 #php ${SYMFONY_HOME}/data/bin/symfony generate:project template-symfony
-#php lib/vendor/symfony/data/bin/symfony generate:project template-symfony
-#php symfony configure:database "mysql:host=localhost;dbname=template-symfony" root 
+#php symfony configure:database "mysql:host=localhost;dbname=template_symfony" root 
 #php symfony generate:app frontend
 #chmod 777 cache/ log/
